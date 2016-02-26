bfffabe3adf341ae5f48e3b2a16ce7c2fe521361
Oleg Bulkin <o.bulkin@gmail.com>
2016-02-26 19:33:10 +0000

    Added some more validations to the place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user, inverse_of: :places
-	validates :name, presence: true
+	validates :name, presence: true, length: {minimum: 4}
+	validates :address, presence: true
+	validates :description, presence: true
 end
