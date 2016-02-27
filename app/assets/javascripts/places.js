4a94222826c874d47739f9bf2a46c409c138ef4a
Oleg Bulkin <o.bulkin@gmail.com>
2016-02-27 06:30:02 +0000

    Added a JS Google Maps element to the view for each individual page

---
+++ app/assets/javascripts/places.js
@@ -0,0 +1,23 @@
+// This is the callback function used by the Google Maps script below
+function initMap() {
+	var mapDiv = document.getElementById('map');
+
+	var myLatLng = {lat: parseFloat(mapDiv.dataset.latitude), lng: parseFloat(mapDiv.dataset.longitude)};
+
+    var map = new google.maps.Map(mapDiv, {
+	    zoom: 15,
+	    center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+    });
+}
+
+// Call the Google Maps API when the page has finished loading via Turbolinks and the view has a map
+$(document).on("page:change", function() {
+	if ($("#map")) {
+		$.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBRTbj1_EjjM29hykn5ZXfGVsvfSAtCgDo&signed_in=true&callback=initMap")
+	}
+});
