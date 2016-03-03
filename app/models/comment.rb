57e202e352b17736e010adf9aae6f479809fa3bc
Oleg Bulkin <o.bulkin@gmail.com>
2016-03-03 02:31:23 +0000

    Updated the individual place view to show ratings as glyphicon stars and tweaked the comment submission form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user, inverse_of: :comments
 	belongs_to :place, inverse_of: :comments
+
+	validates :rating, presence: true
+
+	RATING_MAP = {"Five Stars" => 5, "Four Stars" => 4, "Three Stars" => 3, "Two Stars" => 2, "One Star" => 1}
 end
