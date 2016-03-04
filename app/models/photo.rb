9b5daa4d5571c3a0ade6c155a938892f40f2d298
Oleg Bulkin <o.bulkin@gmail.com>
2016-03-04 22:19:03 +0000

    Lowered the character count on photo captions to 140

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,5 @@ class Photo < ActiveRecord::Base
 	belongs_to :place, inverse_of: :photos
   mount_uploader :image, ImageUploader
   validates :image, presence: :true
-  validates :caption, length: {maximum: 180}
+  validates :caption, length: {maximum: 140}
 end
