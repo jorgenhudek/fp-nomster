5ec957df01eb81713700e0f91dc2eca397ac3bc1
Oleg Bulkin <o.bulkin@gmail.com>
2016-03-05 23:29:21 +0000

    Added logic to comment creation that triggers a notification email

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		Comment.create comment_params.merge({user_id: current_user.id, place_id: params[:place_id]})
+		comment = Comment.create comment_params.merge({user_id: current_user.id, place_id: params[:place_id]})
+    NotificationMailer.comment_added(comment).deliver_later
 		redirect_to place_path(params[:place_id])
 	end
 
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ApplicationMailer
   def comment_added(comment)
     @place = comment.place
-    @place_owner = @place.user
-    mail(to: @place_owner.email, subject: "A comment has been added to your listing for #{@place.name}")
+    place_owner = @place.user
+    mail(to: place_owner.email, subject: "A comment has been added to your listing for #{@place.name}")
   end
 end
